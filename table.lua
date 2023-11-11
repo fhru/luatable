9683e85367eb89d37370c54a45b54ade775c7fd2
Fahru <54230778+fhru@users.noreply.github.com>
2023-11-11 08:41:03 +0700

    Remove data via Discord bot: lavista

--- table.lua
+++ table.lua
@@ -1,6 +1,6 @@
 -- file: module.lua
 local M = {}
-local data = {"mobil","motor","asu","tesits","fas","ivan","ivanasu","sdaop","asu","hasta","lavista"}
+local data = {"mobil","motor","asu","tesits","fas","ivan","ivanasu","sdaop","asu","hasta"}
 
 local function checkOwner(player)
     for _, pp in pairs(data) do
