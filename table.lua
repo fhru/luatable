441af68d634e2be06974424b28749e847abb6899
Fahru <54230778+fhru@users.noreply.github.com>
2023-11-11 08:08:24 +0700

    Update table.lua

--- table.lua
+++ table.lua
@@ -1,1 +1,12 @@
+local M = {}
 data = {"mobil","motor","asu","tesits","fas","ivan","ivanasu","sdaop"}
+
+function checkOwner(player)
+  for _,pp in pairs(data) do
+    if pp == player then
+      return true
+    end
+  end
+end
+
+return M
